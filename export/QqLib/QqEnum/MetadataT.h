d562dabbbc693029747d55d19d13046cdbca6714
pavel.p.vasilenko <binary.x64@gmail.com>
2026-04-04 12:14:32 +0300

    ADD: export/./QqEnum/MetadataT.h

---
+++ export/QqLib/QqEnum/MetadataT.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QList>
+#include <QMap>
+#include <QString>
+#include <QLocale>
+
+
+namespace Qq::Enum
+{
+
+
+template <typename TClass, typename TEnum, typename TInt =int>
+struct MetadataT
+{
+//
+// Types
+//
+protected:
+    using H             = Helper;
+public:
+    using ValueList     = QList<TEnum>;
+    using NameList      = QStringList;
+    using EIMap         = QMap<TEnum,int>;
+
+};
+
+
+} // end of namespace Qq::Enum
