bb94ee1f855aa56847cfa66938b886a566dd69ee
pavel.p.vasilenko <binary.x64@gmail.com>
2026-04-07 11:36:45 +0300

    ADD: export/./QqEnum/CoreT.h

---
+++ export/QqLib/QqEnum/CoreT.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+
+
+namespace Qq::Enum
+{
+
+
+///
+/// \brief The core class for the QQ_ENUM macro.
+/// All work with QqEnum should be here.
+///
+template <typename TClass, typename TEnum, typename TInt =int>
+class CoreT
+{
+
+
+//
+// API:
+//
+public:
+
+};
+
+
+} // namespace Qq::Enum
