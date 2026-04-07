ac76d15f89be773a987b924eb18e3a67adf63faf
pavel.p.vasilenko <binary.x64@gmail.com>
2026-04-07 12:00:08 +0300

    ADD: export/./QqEnum/EatAssignT.h

--- export/QqLib/QqEnum/QqEnum.M.h
+++ export/QqLib/QqEnum/QqEnum.M.h
@@ -1,11 +1,21 @@
 #pragma once
 
+
+#include "./_CompileConfig.h"
+#include QQ_ENUM_QQ_STRING_INCLUDE
+
 #include "../Macros/Variadic/ForEach.M.h"
 #include "../Macros/Expand.M.h"
 #include "../Macros/Cat.M.h"
 
 #include "./EnumItemT.h"
 #include "./CoreT.h"
+#include "./EatAssignT.h"
+
+
+
+#define QP_GET_VALUE_FROM_ENUM_ITEM(ITEM)   _EatAssign{}         << ITEM  , //-V2561
+#define QP_GET_NAME_FROM_ENUM_ITEM(ITEM)    _H::parseEnumValueName(#ITEM) ,
 
 
 #define QQ_ENUM_CORE_AS(Int,Class,...)                                      \
@@ -32,7 +42,22 @@ protected:                                                                  \
     template <typename T_Class, typename T_Enum, typename T_Int>            \
     friend class Qq::Enum::CoreT;                                           \
                                                                             \
-    using _C        = Qq::Enum::CoreT<_Class,_Enum,_Int>;                   \
-    using _D        = Qq::Enum::MetadataT<_Class,_Enum,_Int>;               \
-    using _H        = Qq::Enum::Helper;                                     \
-
+    using _C         = Qq::Enum::CoreT<_Class,_Enum,_Int>;                  \
+    using _D         = Qq::Enum::MetadataT<_Class,_Enum,_Int>;              \
+    using _H         = Qq::Enum::Helper;                                    \
+    using _EatAssign = Qq::Enum::EatAssign<_Enum, _Int>;                    \
+                                                                            \
+    static inline _D _d {                                                   \
+        std::vector<_Enum> {                                                \
+            QQ_PP_FOREACH(                                                  \
+                QP_GET_VALUE_FROM_ENUM_ITEM,                                \
+                __VA_ARGS__                                                 \
+            )                                                               \
+        },                                                                  \
+        std::vector<Qq_string> {                                            \
+            QQ_PP_FOREACH(                                                  \
+                QP_GET_NAME_FROM_ENUM_ITEM,                                 \
+                __VA_ARGS__                                                 \
+            )                                                               \
+        }                                                                   \
+    };                                                                      \
--- export/QqLib/QqEnum/_CompileConfig.h
+++ export/QqLib/QqEnum/_CompileConfig.h
@@ -8,13 +8,13 @@
 
 
 #ifdef QQ_DONT_USE_QT_CONTAINERS
-    #define QqMap                   std::map
-    #define QqList                  std::vector
-    #define Qq_string               std::string
+    #define QqMap                      std::map
+    #define QqList                     std::vector
+    #define Qq_string                  std::string
     #define QQ_ENUM_QQ_STRING_INCLUDE  <string>
 #else
-    #define QqMap                   QMap
-    #define QqList                  QList
-    #define Qq_string               QString
-    #define QQ_ENUM_STRING_INCLUDE  <QString>
+    #define QqMap                      QMap
+    #define QqList                     QList
+    #define Qq_string                  QString
+    #define QQ_ENUM_STRING_INCLUDE     <QString>
 #endif
