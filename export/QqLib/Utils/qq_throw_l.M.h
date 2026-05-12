6a7c01933b29fc2bba7d989827e9f799e1f024e4
pavel.p.vasilenko <binary.x64@gmail.com>
2026-05-12 17:27:22 +0300

    FIX: in qq_throw_l add '\n' before 'EXCEPTION:'

--- export/QqLib/Utils/qq_throw_l.M.h
+++ export/QqLib/Utils/qq_throw_l.M.h
@@ -5,7 +5,7 @@
 
 #define qq_throw_l(EXCEPTION,TEXT)                      \
     {                                                   \
-        std::cerr << "EXCEPTION: ";                     \
+        std::cerr << "\nEXCEPTION: ";                   \
         std::cerr << TEXT << std::endl;                 \
         throw EXCEPTION{ TEXT };                        \
     }
