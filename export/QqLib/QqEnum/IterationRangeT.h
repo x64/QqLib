b7eba69fdee033eb4ab1495b7797826c430d588b
pavel.p.vasilenko <binary.x64@gmail.com>
2026-05-13 17:48:21 +0300

    REN: Qq:Enum::IteratorWrapperT to IterationRangeT; MOD for reverse mode

--- export/QqLib/QqEnum/IterationRangeT.h
+++ export/QqLib/QqEnum/IterationRangeT.h
@@ -9,46 +9,49 @@ namespace Qq::Enum
 
 
 template <typename TClass, typename TEnum, typename TInt =int>
-struct IteratorWrapperT
+struct IterationRangeT
 {
     using Iterator = IteratorT<TClass,TEnum,TInt>;
-    enum : int { c_toIndex = -1 };
+
 //
 // CTORs
 //
-    constexpr IteratorWrapperT()
-        : IteratorWrapperT{ 0 }
+    constexpr IterationRangeT() noexcept
+        : IterationRangeT{ 0 }
     {}
 
-    constexpr IteratorWrapperT(int fromIndex, int toIndex = c_toIndex)
+    constexpr IterationRangeT(int fromIndex, int toIndex = Const::badIndex) noexcept
     {
         using C = CoreT<TClass,TEnum,TInt>;
 
         m_fromIndex = C::toRange(fromIndex, true);
         m_toIndex   = C::toRange(toIndex,   true);
+        m_order     = m_fromIndex > m_toIndex ? -1 : +1;
     }
 
-    constexpr IteratorWrapperT(TEnum from, TEnum to)
+    constexpr IterationRangeT(TEnum from, TEnum to) noexcept
     {
         using D = MetadataT<TClass,TEnum,TInt>;
 
         m_fromIndex = D::indexOf(from);
         m_toIndex   = D::indexOf(to);
+        m_order     = m_fromIndex > m_toIndex ? -1 : +1;
     }
 
     Iterator begin() const noexcept
     {
-        return Iterator{ m_fromIndex };
+        return Iterator{ m_fromIndex, m_toIndex };
     }
 
     Iterator end() const noexcept
     {
-        return Iterator{ 1 + m_toIndex };
+        return Iterator{ m_order + m_toIndex };
     }
 
 private:
     int m_fromIndex;
     int m_toIndex;
+    int m_order;
 };
 
 
