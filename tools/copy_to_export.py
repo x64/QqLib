569894db901fd44bd8edfc2c550240749a252358
pavel.p.vasilenko <binary.x64@gmail.com>
2026-05-30 12:12:27 +0300

    ADD: 'tools/copy_to_export.py' for copy files from 'src' folder to 'export' folder

---
+++ tools/copy_to_export.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import shutil
+import os
+
+fh = open("./files_for_copy_to_export.txt", "rt")
+
+src_dir  = "../src/"
+dest_dir = "../export/QqLib/"
+
+def clean_dest_dir():
+	shutil.rmtree(dest_dir, True)
+	os.makedirs(dest_dir)
+
+def make_dirs(path):
+	os.makedirs(dest_dir + path)
+
+
+clean_dest_dir()
+
+last_folder = ""
+for line in fh.readlines():
+	path = line.strip()
+	if path == "" : continue
+	if path[-1] == "/":
+		make_dirs(path)
+		last_folder = path
+		continue
+	path = last_folder + path
+	print("path: ", path)
+	shutil.copy(src_dir + path, dest_dir + path)
